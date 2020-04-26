8837d60918de87ef23cf8c5d50a56726f6ea8be3
Connor Riordan <criordan@college.harvard.edu>
2020-04-25 23:53:16 -0400

    Shiny commmit after doing a lot of work.

--- final_project/rds/app.R
+++ final_project/rds/app.R
@@ -6,11 +6,21 @@ library(markdown)
 library(ggplot2)
 library(base)
 
+# I needed to use this space in order to read in all of my RDS data so I can
+# use it all in my shiny app.
 
+# This is for all the presidential election data from 2000 to 2016.
 
 pres_res <- readRDS("pres_results.RDS")
 
+# I turned all of the selections from the abb_county column to vectors to make
+# the selection process easier in selectizeInput.
+
 presasvector <- pres_res$abb_county %>% unique()
+
+# This is the data that has all of the different age groups and income levels
+# and how each of them voted durng the election. "ai" stands for age and 
+# income.
 
 age_income_2016 <- readRDS("age_income_2016.RDS")
     
@@ -26,19 +36,38 @@ ai_65_74 <- readRDS("age_income_2016_65_74.RDS")
 
 ai_75_over <- readRDS("age_income_2016_75_over.RDS")
 
+# While I haven't done anything with it yet, this is education data for schools
+# across the country.
+
 education <- readRDS("education.RDS")
+
+# Like I did with the presidential results, I had to put the education column
+# as vectors to make it easier to select the input.
 
 edasvector <- education$name_of_area %>% unique()
 
+# This breaks down the income for each individual county.
+
 income_county <- readRDS("income_county.RDS")
+
+# A trend is certainly appearing. I wanted to make all of the income brackets
+# as vectors as well.
 
 incomeasvector <- income_county$county
 
+# I used options in order to properly load in my data, because otherwise my
+# data wouldn't be loaded in.
+
 options(scipen = 999)
 
+# For this first part of my shiny, I wanted to allow people to compare how 
+# their county voted against everyone else's county, and also how the total
+# votes of counties changed over the course of the five presidential elections.
+# I used a comparative bar plot and a line plot in order to best represent
+# this.
 
 ui <- navbarPage(
-    
+  
     "How Separate Variables Affect Voting Patterns",
     
     tabPanel("Presidential Election Data Since 2000",
@@ -71,6 +100,14 @@ ui <- navbarPage(
                      
                      tableOutput("results2")))
     )),
+    
+# For this next tab, I wanted to look at income data for different age groups
+# for the nation and also by county. The second part does not work yet, but for
+# the first part I had to save each age group as a separate dataframe, and then
+# when the inputs change it switches between data frames. What I plan to do for
+# the rest of the data is to allow people to choose both which county they want
+# to measure and also what y-variable they would like to examine on the graph.
+# So far, I have had no luck, but I will have luck soon.
     
     tabPanel("Income",
              tabsetPanel(
@@ -124,6 +161,11 @@ ui <- navbarPage(
                               )))
              )),
     
+# This education data is not fantastic, so I have downloaded some more to make
+# up for that. My new education data focuses mainly on the graduation rates
+# for school districts across the country, which I think will be invaluable
+# to compare with income and health to inform how people voted in elections.
+
     tabPanel("Education Data",
         
              fluidPage(
@@ -148,6 +190,11 @@ ui <- navbarPage(
         )
     ),
     
+# This is the discussion part and where I am going to run my regression. I
+# haven't had a chance to actually finish running my regression and put it in
+# my data because I had to spend all night cleaning it up in order to actually
+# join the presidential election data and the income data, which are the two
+# sets that I want to join for the regression.
     
     tabPanel("Running Regressions and Discussion",
              
@@ -180,6 +227,8 @@ ui <- navbarPage(
              
              )
 )
+
+# These are all the outputs for my data.
 
 server <- function(input, output) {
     
@@ -268,7 +317,8 @@ server <- function(input, output) {
         
     })
     
-    
+# I wanted to insert an image because I haven't made a video yet!
+  
     output$image <- renderImage({
         
         filename <- normalizePath(file.path('.', paste('image', '.jpg', sep='')))
